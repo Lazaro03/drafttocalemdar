d25f888ffb5cdfbf8c289b609ae060a1ebbab35f
Lazaro03 <95556479+Lazaro03@users.noreply.github.com>
2022-07-04 23:23:05 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,4 +1,4 @@
 api_hash = "39ad6135ac9b75a47f2d1804c092c8f8"
 api_id = 11405596
 bot_token = "5539723659:AAFPsXhmAvsQLfLvNMjkUf7XnJNDaIyBfMY"
-bot_admin_group = -1001714599568
+bot_admin_group = -1001575144727
